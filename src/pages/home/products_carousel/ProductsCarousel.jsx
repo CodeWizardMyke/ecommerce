25557dbaae83c7213d62a7b5f2891b278fc45aee
myke william oliveira feitosa <maikewilliam.dev@gmail.com>
2024-06-17 13:35:00 -0300

    Resolved react error regarding change in product array

--- src/pages/home/products_carousel/ProductsCarousel.jsx
+++ src/pages/home/products_carousel/ProductsCarousel.jsx
@@ -1,9 +1,17 @@
+import { useState } from 'react'
 import './productsCarousel.css'
 
 function ProductsCarousel({products, title}) {
+   const [cartItem, setCartItem] = useState([])
 
+   const insertItemCart =(item)=>{
+    const {id, title, images, price} = item
+    const handdlerItem = {id:id, title:title, images:images, price:price}
+
+  }
+  console.log(cartItem)
   return (
-     <>
+    <>
      <div className='title-sales'>
       <h2>{title}</h2>
      </div>
@@ -23,7 +31,8 @@ function ProductsCarousel({products, title}) {
                       <span className='product-price-pix'>R$: ${  item.price - (( item.price * 15 ) / 100 ).toFixed(2) }</span>
                       <span className='text-pix'>no Pix.</span>
                     </div>
-                    <button className='btn btn-secondary'>Comprar</button>
+                    <button className='btn btn-success'>Comprar</button>
+                    <button className='btn btn-outline-dark' onClick={()=>insertItemCart(item)} >Carrinho</button>
                   </div>
                 </div>
             ))
